043f26a3ba84ff06004e888d078ef813a30938f8
Darwish Quiambao <darwishjeric.quiambao@sjsu.edu>
2022-02-24 01:32:37 -0800

    added Message class for Milestone 2;

--- src/Client.java
+++ src/Client.java
@@ -2,16 +2,23 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Client {
     public static void main(String[] args) throws IOException {
 
         // socket - without port number, OS chooses a random one available
-
         DatagramSocket datagramSocket = new DatagramSocket();
-
+//        System.out.println("Type");
+//        Scanner sc = new Scanner(System.in);
+//        sc.nextLine();
+//        while()
         String message = "hello";
         byte[] data = message.getBytes();
+
 
         DatagramPacket packet = new DatagramPacket(
                 data,
---
+++ src/Message.java
@@ -0,0 +1,32 @@
+public class Message {
+
+    private int messageType;
+    private String userName;
+    private String message;
+    public Message(){};
+
+    // Setters & Getters
+    public int getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(int messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
